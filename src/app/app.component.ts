14cf91e51daea46eebe3da88b48cb3cf2f1e8c65
vsgitdev <vasiliki.tsiotinou@gmail.com>
2024-11-04 00:52:32 +0200

    Directives 1 step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { MasterComponent } from './components/master/master.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RolesComponent } from './components/roles/roles.component';
@@ -5,7 +6,7 @@ import { RolesComponent } from './components/roles/roles.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RolesComponent],
+  imports: [RouterOutlet,RolesComponent, MasterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../../model/interface/role';
 
 @Component({
   selector: 'app-roles',
@@ -8,24 +10,52 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
-  firstName: string = "Angular Tutorial";
-  angularVersion = "Version 18";  
+export class RolesComponent implements OnInit{
+  
+// constructor(private http: HttpClient) {
+  
+// }
+roleList: IRole [] = [];
+http = inject(HttpClient);
 
-  version: number = 18;
-  isActive: boolean = false;
-  curentDate: Date = new Date();
-  inputType: string = "button";  
-  selectedState: string = '';
+ngOnInit(): void {
+    this.getAllRoles()
+}
+
+getAllRoles() {
+  this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res:any)=>{
+    this.roleList = res.data;
+
+  })
+}
 
 
-  showWelcomeAlert (){
-    alert("Welcome to Angular 18 Tutorial")
-  }
 
-  showMessage (message: string) {
-    alert(message)
-  }
+
+
+
+
+
+
+
+
+  // firstName: string = "Angular Tutorial";
+  // angularVersion = "Version 18";  
+
+  // version: number = 18;
+  // isActive: boolean = false;
+  // curentDate: Date = new Date();
+  // inputType: string = "button";  
+  // selectedState: string = '';
+
+
+  // showWelcomeAlert (){
+  //   alert("Welcome to Angular 18 Tutorial")
+  // }
+
+  // showMessage (message: string) {
+  //   alert(message)
+  // }
 
 
 }
