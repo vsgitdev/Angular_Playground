01a9ee31c59f68b8e94162c7739141a8892d2426
vsgitdev <vasiliki.tsiotinou@gmail.com>
2024-11-06 22:56:23 +0200

    Routing -> routes, routeroutlet, routerLink, routerLinkActive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { MasterComponent } from './components/master/master.component';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { RolesComponent } from './components/roles/roles.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RolesComponent, MasterComponent],
+  imports: [RouterOutlet,RolesComponent, MasterComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
